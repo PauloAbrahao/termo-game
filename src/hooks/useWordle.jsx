047894e6fae0d86080ede58d4f6609d2da0a1bf5
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2022-05-04 17:17:27 -0300

    lesson 5 ok

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -10,7 +10,7 @@ const useWordle = (solution) => {
   // format a guess into an array of letter objects 
   // e.g. [{key: 'a', color: 'yellow'}]
   const formatGuess = () => {
-    
+    // console.log('formatting the guess - ', currentGuess);
   }
 
   // add a new guess to the guesses state
@@ -23,17 +23,35 @@ const useWordle = (solution) => {
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
+    // console.log('key pressed - ', key)
+    if (key === "Enter") {
+
+      // only add guess if the turn is less than 5
+      if (turn > 5) {
+        console.log('you have reached the max number of turns')
+        return
+      }
+
+      // do not allow dupplicate words
+      if (history.includes(currentGuess)) {
+        console.log('you have already guessed this word');
+        return 
+      }
+
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log('word must be 5 characters long');
+        return
+      }
+    }
+    formatGuess()
     if (key === 'Backspace') {
-      setCurrentGuess((prev) => {
-        return prev.slice(0, -1)
-      })
-      return 
+      setCurrentGuess(prev => prev.slice(0, -1))
+      return
     }
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
-        setCurrentGuess((prev) => {
-          return prev + key
-        })
+        setCurrentGuess(prev => prev + key)
       }
     }
   }
