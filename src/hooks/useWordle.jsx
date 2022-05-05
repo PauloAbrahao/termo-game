3f8ac778a67d1d55ea1e72ad345e5fd74106d137
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2022-05-05 16:54:46 -0300

    lesson 7

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -1,62 +1,85 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const useWordle = (solution) => {
-  const [turn, setTurn] = useState(0) 
-  const [currentGuess, setCurrentGuess] = useState('')
-  const [guesses, setGuesses] = useState([]) // each guess is an array
-  const [history, setHistory] = useState([]) // each guess is a string
-  const [isCorrect, setIsCorrect] = useState(false)
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([]); // each guess is an array
+  const [history, setHistory] = useState([]); // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false);
 
-  // format a guess into an array of letter objects 
+  // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
   const formatGuess = () => {
     // console.log('formatting the guess - ', currentGuess);
-  }
+
+    let solutionArray = [...solution];
+
+    let formattedGuess = [...currentGuess].map((l) => {
+      return { key: l, color: "grey" };
+    });
+
+    //find any green letters
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray[i] === l.key) {
+        formattedGuess[i].color = "green";
+        solutionArray[i] = null;
+      }
+    });
+
+    // find any yellow colors
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray.includes(l.key) && l.color !== "green") {
+        formattedGuess[i].color = "yellow";
+        solutionArray[solutionArray.indexOf(l.key)] = null;
+      }
+    });
+    return formattedGuess;
+  };
 
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
   // add one to the turn state
-  const addNewGuess = () => {
-
-  }
+  const addNewGuess = () => {};
 
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
     // console.log('key pressed - ', key)
     if (key === "Enter") {
-
       // only add guess if the turn is less than 5
       if (turn > 5) {
-        console.log('you have reached the max number of turns')
-        return
+        console.log("you have reached the max number of turns");
+        return;
       }
 
       // do not allow dupplicate words
       if (history.includes(currentGuess)) {
-        console.log('you have already guessed this word');
-        return 
+        console.log("you have already guessed this word");
+        return;
       }
 
       // check word is 5 chars long
       if (currentGuess.length !== 5) {
-        console.log('word must be 5 characters long');
-        return
+        console.log("word must be 5 characters long");
+        return;
       }
     }
-    formatGuess()
-    if (key === 'Backspace') {
-      setCurrentGuess(prev => prev.slice(0, -1))
-      return
+
+    const formatted = formatGuess();
+    console.log(formatted);
+
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => prev.slice(0, -1));
+      return;
     }
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
-        setCurrentGuess(prev => prev + key)
+        setCurrentGuess((prev) => prev + key);
       }
     }
-  }
+  };
 
-  return {turn, currentGuess, guesses, isCorrect, handleKeyup}
-}
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+};
 
-export default useWordle
+export default useWordle;
