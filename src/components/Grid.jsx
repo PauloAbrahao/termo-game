ae47579584cedeef80e41667b16fb4f72ecb94bb
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2022-05-11 15:35:58 -0300

    lesson 10

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,6 +7,9 @@ const Grid = ({ currentGuess, guesses, turn }) => {
     <div>
 
       {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess}/>;
+        }
         return <Row key={i} guess={g} />;
       })}
 
